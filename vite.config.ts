c8d064917a6d288b43b55856a4380d8fd2e9b04c
Gaurav Soni <your.email@example.com>
2026-04-27 07:58:06 +0000

    chore: fix chunk size warning with code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Ye third-party libraries (jaise react, supabase) ko alag file mein daal dega
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
+});
